cfe0fdab6d5dcdf15a579aa442ae7dc554237c88
snakepatch53 <snakepatch53@gmail.com>
2023-12-06 22:08:56 -0500

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   public/css.panel/general.css
    	modified:   public/css.panel/paquetes.css
    	modified:   src/assets/db.sql
    	new file:   src/assets/diagrama.png

--- src/assets/db.sql
+++ src/assets/db.sql
@@ -1,8 +1,14 @@
+-- desactivar llaves foraneas
+SET
+    foreign_key_checks = 0;
+
 DROP TABLE IF EXISTS info;
 
 CREATE TABLE info (
     info_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     info_name VARCHAR(50),
+    info_whatsapp VARCHAR(50),
+    info_email VARCHAR(50),
     info_last VARCHAR(50)
 ) ENGINE INNODB;
 
@@ -10,6 +16,8 @@ INSERT INTO
     info
 VALUES
     (1, 'Voy Llevando', '2023-01-01 00:00:00');
+
+DROP TABLE IF EXISTS locker;
 
 -- locker -> hace referencia a las casas donde pueden enviar sus paquetes
 CREATE TABLE locker (
@@ -29,6 +37,7 @@ VALUES
 
 DROP TABLE IF EXISTS user;
 
+-- 1 => Super Admin, 2 => Admin, 3 => Currier, 4 => User
 CREATE TABLE user (
     user_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     user_name VARCHAR(50),
@@ -52,9 +61,7 @@ INSERT INTO
         user_lastname,
         user_dni,
         user_pass,
-        user_type,
-        user_created,
-        user_last
+        user_type
     )
 VALUES
     (
@@ -62,19 +69,24 @@ VALUES
         'Admin',
         'admin',
         'admin',
-        1,
-        '2023-01-01 00:00:00',
-        '2023-01-01 00:00:00'
+        1
     ),
     (
         'Admin',
         'Admin',
         'd295336',
         'MV9~GV7Bq~VL',
-        1,
-        '2023-01-01 00:00:00',
-        '2023-01-01 00:00:00'
+        1
+    ),
+    (
+        'Harold Hernandez',
+        '0704870377',
+        'snakepatch53@gmail.com',
+        '123',
+        4
     );
+
+DROP TABLE IF EXISTS shipment;
 
 CREATE TABLE shipment (
     shipment_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
@@ -86,21 +98,44 @@ CREATE TABLE shipment (
     FOREIGN KEY (user_id) REFERENCES user(user_id)
 ) ENGINE INNODB;
 
+DROP TABLE IF EXISTS alert;
+
+-- ESTADOS: 0 => Revision, 1 => Pendiente, 2 => Pagado, 3 => Finalizado
 CREATE TABLE alert (
     alert_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     alert_buyer_name VARCHAR(50),
     alert_carrier_name VARCHAR(50),
     alert_tracking_code VARCHAR(50),
     alert_bill_file VARCHAR(50),
+    alert_status INT DEFAULT 0,
     alert_last VARCHAR(50),
     alert_created VARCHAR(50),
     user_id INT,
-    shipment_id INT,
     locker_id INT,
     FOREIGN KEY (user_id) REFERENCES user(user_id),
-    FOREIGN KEY (shipment_id) REFERENCES shipment(shipment_id),
     FOREIGN KEY (locker_id) REFERENCES locker(locker_id)
 ) ENGINE INNODB;
+
+INSERT INTO
+    alert (
+        alert_buyer_name,
+        alert_carrier_name,
+        alert_tracking_code,
+        alert_bill_file,
+        user_id,
+        locker_id
+    )
+VALUES
+    (
+        'Harold Hernandez',
+        'Amazon',
+        '35456343',
+        null,
+        1,
+        1
+    );
+
+DROP TABLE IF EXISTS tracking;
 
 CREATE TABLE tracking (
     tracking_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
@@ -109,6 +144,8 @@ CREATE TABLE tracking (
     alert_id INT,
     FOREIGN KEY (alert_id) REFERENCES alert(alert_id)
 ) ENGINE INNODB;
+
+DROP TABLE IF EXISTS category;
 
 CREATE TABLE category (
     category_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
@@ -124,6 +161,8 @@ VALUES
     (2, 'Vestimenta'),
     (3, 'Respuestos'),
     (4, 'Documentos');
+
+DROP TABLE IF EXISTS subcategory;
 
 CREATE TABLE subcategory (
     subcategory_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
@@ -162,9 +201,15 @@ VALUES
     ('Libros', 4),
     ('Otros', 4);
 
+DROP TABLE IF EXISTS product;
+
 CREATE TABLE product (
     product_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     product_name VARCHAR(50),
+    product_description TEXT,
+    product_price DOUBLE,
+    product_weight DOUBLE,
+    product_quantity INT,
     product_last VARCHAR(50),
     product_created VARCHAR(50),
     subcategory_id INT,
@@ -172,3 +217,19 @@ CREATE TABLE product (
     FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id),
     FOREIGN KEY (alert_id) REFERENCES alert(alert_id)
 ) ENGINE INNODB;
+
+DROP TABLE IF EXISTS shipment_product;
+
+CREATE TABLE shipment_product (
+    shipment_product_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    shipment_product_last VARCHAR(50),
+    shipment_product_created VARCHAR(50),
+    shipment_id INT,
+    product_id INT,
+    FOREIGN KEY (shipment_id) REFERENCES shipment(shipment_id),
+    FOREIGN KEY (product_id) REFERENCES product(product_id)
+) ENGINE INNODB;
+
+-- activar llaves foraneas
+SET
+    foreign_key_checks = 1;
